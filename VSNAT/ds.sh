76c6e93b27c761835f8b4c48ed7639eccb6b2d35
U-AUTONAVI\zhenhua.lei <zhenhua.lei@AUTONAV-SAOQ3FT.autonavi.com>
2014-11-17 18:08:32 +0800

    step by step
    http://www.austintek.com/LVS/LVS-HOWTO/mini-HOWTO/LVS-mini-HOWTO.html#LVS-NAT%20by%20hand

--- VSNAT/ds.sh
+++ VSNAT/ds.sh
@@ -10,3 +10,47 @@ sudo ipvsadm -a -t 10.0.0.10:8689 -r 10.0.1.3:8964 -m
 
 sudo iptables -I INPUT -p tcp -m tcp --dport 8689 -j ACCEPT
 sudo service iptables save
+
+#------mini-HOWTO-setup-LVS-NAT-director----------
+
+#set ip_forward ON for vs-nat director (1 on, 0 off).
+cat /proc/sys/net/ipv4/ip_forward
+sudo echo "1" >/proc/sys/net/ipv4/ip_forward
+
+#director is gw for realservers
+#turn OFF icmp redirects (1 on, 0 off)
+sudo echo "0" >/proc/sys/net/ipv4/conf/all/send_redirects
+cat       /proc/sys/net/ipv4/conf/all/send_redirects
+sudo echo "0" >/proc/sys/net/ipv4/conf/default/send_redirects
+cat       /proc/sys/net/ipv4/conf/default/send_redirects
+sudo echo "0" >/proc/sys/net/ipv4/conf/eth0/send_redirects
+cat       /proc/sys/net/ipv4/conf/eth0/send_redirects
+
+#setup VIP
+sudo /sbin/ifconfig eth1:110 192.168.2.110 broadcast 192.168.2.255 netmask 255.255.255.0
+
+#set default gateway
+sudo /sbin/route add default gw 192.168.2.254 netmask 0.0.0.0 metric 1
+
+#clear ipvsadm tables
+sudo /sbin/ipvsadm -C
+
+#install LVS services with ipvsadm
+#add telnet to VIP with rr sheduling
+sudo /sbin/ipvsadm -A -t 192.168.2.110:8689 -s rr
+
+#first realserver
+#forward telnet to realserver 192.168.1.11 using LVS-NAT (-m), with weight=1
+sudo /sbin/ipvsadm -a -t 192.168.2.110:8689 -r 192.168.1.11:8964 -m -w 1
+#check that realserver is reachable from director
+ping -c 1 192.168.1.11
+
+#second realserver
+#forward telnet to realserver 192.168.1.12 using LVS-NAT (-m), with weight=1
+sudo /sbin/ipvsadm -a -t 192.168.2.110:8689 -r 192.168.1.12:8964 -m -w 1
+#checking if realserver is reachable from director
+ping -c 1 192.168.1.12
+
+#list ipvsadm table
+sudo /sbin/ipvsadm
+#------mini-HOWTO-setup-LVS-NAT-director----------
--- VSNAT/rs.sh
+++ VSNAT/rs.sh
@@ -6,6 +6,22 @@ sudo iptables -I INPUT -p tcp -m tcp --dport 8964 -j ACCEPT
 
 sudo service iptables save
 
+#---------mini-HOWTO-setup-LVS-NAT-realserver-------
+#installing default gw 192.168.1.9 for vs-nat'
+sudo /sbin/route add default gw 192.168.1.9
+#show routing table
+sudo netstat -rn
 
+#checking if DEFAULT_GW is reachable
+ping -c 1 192.168.1.9
+
+#looking for VIP on director from realserver
+ping -c 1 192.168.2.110
+
+#set_realserver_ip_forwarding to OFF (1 on, 0 off).
+sudo echo "0" >/proc/sys/net/ipv4/ip_forward
+cat       /proc/sys/net/ipv4/ip_forward
+
+#---------mini-HOWTO-setup-LVS-NAT-realserver-------
 
 nohup ncat -lk 8964 -c "echo $(hostname) say hello !! " &
