bf180ebbbcd15be6778ccfbbdceb152dd837a30b
zhenhua.lei <zhenhua.lei@alibaba-inc.com>
2015-04-29 10:31:58 +0800

    cute to kernel configuration

--- VSDR/rs.install.sh
+++ VSDR/rs.install.sh
@@ -3,6 +3,6 @@
 sudo yum install man nc nmap vim traceroute tcpdump -y
 
 sudo iptables -I INPUT -p tcp -m tcp --dport 8964 -j ACCEPT
-sudo iptables -t nat -A PREROUTING -p tcp -d 10.0.0.10 --dport 8964 -j REDIRECT --to-port 8964
+#sudo iptables -t nat -A PREROUTING -p tcp -d 10.0.0.10 --dport 8964 -j REDIRECT --to-port 8964
 sudo service iptables save
 
--- VSDR/rs.sh
+++ VSDR/rs.sh
@@ -2,4 +2,6 @@
 
 sudo ip addr add 10.0.0.10 label lo:vip dev lo
 
+sudo sh -c "echo 1 > /proc/sys/net/ipv4/conf/all/arp_ignore;echo 1 > /proc/sys/net/ipv4/conf/lo/arp_ignore;echo 2 > /proc/sys/net/ipv4/conf/lo/arp_announce;echo 2 > /proc/sys/net/ipv4/conf/all/arp_announce"
+
 nohup ncat -lk 8964 -c "echo $(hostname) say hello !! " &
