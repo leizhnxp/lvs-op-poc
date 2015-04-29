3ff1de4fcaa970fb319bfbedba0488022c6b2658
U-AUTONAVI\zhenhua.lei <zhenhua.lei@AUTONAV-SAOQ3FT.autonavi.com>
2015-04-29 14:11:08 +0800

    auto correct

--- VSDR/rs.sh
+++ VSDR/rs.sh
@@ -5,3 +5,4 @@ sudo ip addr add 10.0.0.10 label lo:vip dev lo
 sudo sh -c "echo 1 > /proc/sys/net/ipv4/conf/all/arp_ignore;echo 1 > /proc/sys/net/ipv4/conf/lo/arp_ignore;echo 2 > /proc/sys/net/ipv4/conf/lo/arp_announce;echo 2 > /proc/sys/net/ipv4/conf/all/arp_announce"
 
 nohup ncat -lk 8964 -c "echo $(hostname) say hello !! " &
+#sudo rpm -ivh /vagrant/jdk-7u60-linux-x64.rpm
